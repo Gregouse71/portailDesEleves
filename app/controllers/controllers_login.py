abfba9a02c6405a53d74abcd4ecb979dbeb1cbc2
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-01-06 18:18:20 +0100

    Controller de login plus cohérent

--- app/controllers/controllers_login.py
+++ app/controllers/controllers_login.py
@@ -1,63 +1,39 @@
-# views_utilisateurs.py
 from flask import Blueprint, request, jsonify
-from flask_login import current_user # necessaire pour tester l'authentification
-from flask_login import login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
-
+from flask_login import current_user, login_user, logout_user, login_required
+from ..models import db, Utilisateur
 from app.services.services_login import send_reset_mail, set_new_password, check_pw
 
-# Création du Blueprint "users"
 controllers_login = Blueprint('controllers_login', __name__)
-from ..models import db, Utilisateur
 
-@controllers_login.route('est_auth', methods=['GET'])
+@controllers_login.route('/est_auth', methods=['GET'])
 def est_auth():
-    """
-    Renvoie True si l'utilisateur est connecte, False sinon
-    """
-    if current_user.is_authenticated : 
-        return jsonify({"etat_connexion": True}), 200
-    else :
-        return jsonify({"etat_connexion": False}), 200
+    return jsonify({"etat_connexion": current_user.is_authenticated}), 200
 
-
-@controllers_login.route('current_user_id', methods=['GET'])
+@controllers_login.route('/current_user_id', methods=['GET'])
 @login_required
-def current_user_id():
-    """
-    Renvoie l'id de l'utilisateur connecte, une erreur 405 s'il n'est pas connecte
-    """
-    if current_user.is_authenticated : 
-        return jsonify({"id_utilisateur": current_user.id}), 200
-    else :
-        return jsonify({"id_utilisateur": None}), 405
+def get_current_user_id():
+    # Because @login_required is used, we know the user is authenticated here
+    return jsonify({"id_utilisateur": current_user.id}), 200
 
-
-# route pour se connecter, executee par React
-@controllers_login.route('connexion', methods=['POST'])
+@controllers_login.route('/connexion', methods=['POST'])
 def connexion():
-    data = request.get_json()
+    data = request.get_json() or {}
     username = data.get('username')
     password = data.get('password')
 
     utilisateur = Utilisateur.query.filter_by(nom_utilisateur=username).first()
+    
     if utilisateur and check_pw(utilisateur, password):
-        login_user(utilisateur)  # Connecte l'utilisateur
+        login_user(utilisateur, remember=True)
         return jsonify({"connecte": True}), 200
-    else :
-        return jsonify({"connecte": False}), 401
+    
+    return jsonify({"connecte": False, "error": "Identifiants invalides"}), 401
 
-
-# se deconnecter    
-@controllers_login.route('deconnexion', methods=['POST'])
+@controllers_login.route('/deconnexion', methods=['POST'])
 @login_required
 def deconnexion():
     logout_user()
-    if current_user.is_authenticated :
-        return jsonify({'connecte':True}), 500
-    else :
-        return jsonify({'connecte':False}), 200
-
+    return jsonify({'connecte': False}), 200
 
 @controllers_login.route('reset', methods=['POST'])
 def reset_mail():
