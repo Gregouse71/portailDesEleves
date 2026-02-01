9ccf00edd37d116a8af7f2493fa187a575745144
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-02-01 16:51:14 +0100

    Mise à jour mailing lists

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -5,14 +5,14 @@ import { obtenirPublicationsRecentes } from '../../api/api_publications';
 
 function Home() {
     const mailingLists = [
-        { name: 'Élèves 1A IC (P24)', email: 'promo-ic24-tous@groupes.mines-paristech.fr' },
-        { name: 'Élèves 2A IC (P23)', email: 'promo-ic23-hors-cesure@groupes.mines-paristech.fr' },
-        { name: 'Élèves Césuriens IC (P22)', email: 'promo-ic23-cesure@groupes.mines-paristech.fr' },
-        { name: 'Élèves 2A IC et Césuriens IC (P22 + P23)', email: 'promo-ic23-tous@groupes.mines-paristech.fr' },
-        { name: 'Élèves 3A IC (P21)', email: 'promo-ic22-tous@groupes.mines-paristech.fr' },
-        { name: 'Élèves 1A ISUP (I24)', email: 'promo-isupfere24@groupes.mines-paristech.fr' },
-        { name: 'Élèves 2A ISUP (I23)', email: 'promo-isupfere23@groupes.mines-paristech.fr' },
-        { name: 'Élèves 3A ISUP (I22)', email: 'promo-isupfere22@groupes.mines-paristech.fr' },
+        { name: 'Élèves 1A IC (P25)', email: 'promo-ic25-tous@groupes.mines-paristech.fr' },
+        { name: 'Élèves 2A IC (P24)', email: 'promo-ic24-hors-cesure@groupes.mines-paristech.fr' },
+        { name: 'Élèves Césuriens IC (P23)', email: 'promo-ic24-cesure@groupes.mines-paristech.fr' },
+        { name: 'Élèves 2A IC et Césuriens IC (P23 + P24)', email: 'promo-ic24-tous@groupes.mines-paristech.fr' },
+        { name: 'Élèves 3A IC (P22)', email: 'promo-ic23-tous@groupes.mines-paristech.fr' },
+        { name: 'Élèves 1A ISUP (I25)', email: 'promo-isupfere25@groupes.mines-paristech.fr' },
+        { name: 'Élèves 2A ISUP (I24)', email: 'promo-isupfere24@groupes.mines-paristech.fr' },
+        { name: 'Élèves 3A ISUP (I23)', email: 'promo-isupfere23@groupes.mines-paristech.fr' },
     ];
 
     const contacts = [
