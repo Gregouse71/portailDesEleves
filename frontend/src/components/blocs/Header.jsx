a5d345b4c5af32e10e1b99670e187a52e72df338
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-03-16 00:10:19 +0100

    Header mieux

--- frontend/src/components/blocs/Header.jsx
+++ frontend/src/components/blocs/Header.jsx
@@ -56,11 +56,15 @@ export default function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="w-100 d-flex flex-column flex-md-row align-items-md-center gap-2">
-                        <NavDropdown title="Menu" id="basic-nav-dropdown">
+                        {/* Menu Dropdown for desktop */}
+                        <NavDropdown title="Menu" id="basic-nav-dropdown" className="d-none d-md-block">
                             <NavDropdown.Item onClick={() => navigate("/")}>Accueil</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => navigate("/assos")}>Associations</NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => navigate("/trombi")}>Trombinoscope</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => navigate("/assos/planning")}>Planning associatif</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => navigate("/trombi")}>Trombinoscope</NavDropdown.Item>
+                            {(octoPermission || octoAdminPermission || bieroPermission || bieroAdminPermission)
+                                && <NavDropdown.Item variant="info" size="sm" onClick={() => navigate("/soifguard")}>Soifguard</NavDropdown.Item>}
                             <NavDropdown.Divider />
                             <NavDropdown.Item onClick={() => navigate("/vendomes")}>Vendômes</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => navigate("/palums")}>Palums</NavDropdown.Item>
@@ -70,11 +74,23 @@ export default function Header() {
                             <NavDropdown.Item onClick={() => navigate("/jeux/2048")}>2048</NavDropdown.Item>
                         </NavDropdown>
 
-                        <Button variant="danger" size="sm" href="https://discord.gg/3MtV8cgTRu">Serveur Discord</Button>
+                        {/* Expanded Menu for mobile */}
+                        <div className="d-md-none d-flex flex-column">
+                            <Nav.Link onClick={() => navigate("/")}>Accueil</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/assos")}>Associations</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/assos/planning")}>Planning associatif</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/trombi")}>Trombinoscope</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/vendomes")}>Vendômes</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/palums")}>Palums</Nav.Link>
+                            <Nav.Link href="https://docs.google.com/spreadsheets/d/1ajgPhZc1xKjB0WZGNqucb5h47aMxdxAfbpOEtxj0Uis/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Sheet des stages</Nav.Link>
+                            <Nav.Link href="https://demarches.portail.minesparis.psl.eu/ordre-de-mission-apprenant/" target="_blank" rel="noopener noreferrer">Réservation véhicule des Mines</Nav.Link>
+                            <Nav.Link onClick={() => navigate("/jeux/2048")}>2048</Nav.Link>
+                        </div>
 
-                        <div className="d-flex flex-column flex-md-row gap-2">
-                            {(octoPermission || octoAdminPermission || bieroPermission || bieroAdminPermission) && <Button variant="info" size="sm" onClick={() => navigate("/soifguard")}>Soifguard</Button>}
-                            {userData.is_superuser && <Button variant="danger" size="sm" onClick={() => navigate("/administration")}>Administration</Button>}
+                        <Button variant="danger" size="sm" href="https://discord.gg/3MtV8cgTRu" className="w-auto mx-auto mx-md-0">Serveur Discord</Button>
+
+                        <div className="d-flex flex-row flex-wrap flex-md-row gap-2 w-auto mx-auto mx-md-0">
+
                         </div>
 
                         <div className="d-none d-md-block flex-grow-1"></div>
@@ -92,6 +108,7 @@ export default function Header() {
                         <NavDropdown title={userData ? userData.nom_utilisateur : "Chargement..."} id="user-nav-dropdown" align="end">
                             <NavDropdown.Item onClick={() => navigate(`utilisateur/${userData.id}`)}>Ma page</NavDropdown.Item>
                             <NavDropdown.Item onClick={() => handleLogout()}>Se déconnecter</NavDropdown.Item>
+                            {userData.is_superuser && <NavDropdown.Item variant="danger" size="sm" onClick={() => navigate("/administration")}>Administration</NavDropdown.Item>}
                             <NavDropdown.Divider />
                             <div className="d-flex justify-content-between align-items-center px-3">
                                 Thème
