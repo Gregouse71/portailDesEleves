343e8fdbdf753a053d284a31a3c6181906a32c81
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-04-01 22:51:39 +0200

    Chargement plus rapide pour première connexion

--- frontend/src/Protected.jsx
+++ frontend/src/Protected.jsx
@@ -9,10 +9,16 @@ const ProtectedContext = createContext();
 export function ProtectedRoute() {
     const location = useLocation();
 
-    const { data: id, isLoading } = useQuery({
+    const { data: id, isLoading, isError, error } = useQuery({
         queryKey: ['id'],
-        queryFn: obtenirIdUser
-    })
+        queryFn: obtenirIdUser,
+        retry: (failureCount, error) => {
+            console.log(error)
+            if (error?.response?.status === 401) return false;
+            return failureCount < 3;
+        }
+    });
+    const isUnauthorized = isError && error?.response?.status === 401;
 
     const { data: userData = {
         promotion: 2,
@@ -32,7 +38,9 @@ export function ProtectedRoute() {
 
     if (isLoading) return <div>Chargement ...</div>;
 
-    if (!id) return <Navigate to="/login" state={{ from: location }} replace />;
+    if (!id || isUnauthorized || isError) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
 
     if (isLoadingUser) return <div>Chargement ...</div>;
 
