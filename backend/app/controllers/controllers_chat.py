c0c54b10df99991ec7262dd43ca82e62a22e24bf
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-07-12 17:42:43 +0200

    Messages chat cachés

--- backend/app/controllers/controllers_chat.py
+++ backend/app/controllers/controllers_chat.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from sqlalchemy import desc
+
+from datetime import date
 
 from app.models.models_chat import Message
 
@@ -12,8 +14,13 @@ def more_chat_message(last_sent: int):
     """
     Renvoie plus de messages à afficher, le dernier vu étant *last_sent*
     """
-    if last_sent == 0:
-        messages = Message.query.order_by(desc(Message.id)).limit(100).all()
-    else:
-        messages = Message.query.filter(Message.id < last_sent).order_by(desc(Message.id)).limit(100).all()
+    query = Message.query
+    if last_sent > 0:
+        query = query.filter(Message.id < last_sent)
+    if not current_user.est_baptise:
+        now = date.today()
+        rentree = date(now.year, 8, 28) # Messages envoyés avant le 28 aout invisibles
+        query = query.filter(Message.date >= rentree)
+
+    messages = query.order_by(desc(Message.id)).limit(100).all()
     return jsonify([message.to_dict() for message in messages[::-1]])
