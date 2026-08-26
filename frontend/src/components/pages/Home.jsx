5d6e4b3251d63b54691e548702bcb37130ed6501
Vianney Decroux <vianney.decroux@etu.minesparis.psl.eu>
2026-08-26 22:59:51 +0200

    MAJ home P26

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -5,14 +5,14 @@ import { obtenirPublicationsRecentes } from '../../api/api_publications';
 
 function Home() {
     const mailingLists = [
-        { name: 'Élèves 1A IC (P25)', email: 'promo-ic25-tous@minesparis.psl.eu' },
-        { name: 'Élèves 2A IC (P24)', email: 'promo-ic24-hors-cesure@minesparis.psl.eu' },
-        { name: 'Élèves Césuriens IC (P23)', email: 'promo-ic24-cesure@minesparis.psl.eu' },
-        { name: 'Élèves 2A IC et Césuriens IC (P23 + P24)', email: 'promo-ic24-tous@minesparis.psl.eu' },
-        { name: 'Élèves 3A IC (P22)', email: 'promo-ic23-tous@minesparis.psl.eu' },
-        { name: 'Élèves 1A ISUP (I25)', email: 'promo-isupfere25@minesparis.psl.eu' },
-        { name: 'Élèves 2A ISUP (I24)', email: 'promo-isupfere24@minesparis.psl.eu' },
-        { name: 'Élèves 3A ISUP (I23)', email: 'promo-isupfere23@minesparis.psl.eu' },
+        { name: 'Élèves 1A IC (P26)', email: 'promo-ic26-tous@minesparis.psl.eu' },
+        { name: 'Élèves 2A IC (P25)', email: 'promo-ic25-hors-cesure@minesparis.psl.eu' },
+        { name: 'Élèves Césuriens IC (P24)', email: 'promo-ic25-cesure@minesparis.psl.eu' },
+        { name: 'Élèves 2A IC et Césuriens IC (P24 + P25)', email: 'promo-ic25-tous@minesparis.psl.eu' },
+        { name: 'Élèves 3A IC (P23 + P24)', email: 'promo-ic24-tous@minesparis.psl.eu' },
+        { name: 'Élèves 1A ISUP (I26)', email: 'promo-isupfere26@minesparis.psl.eu' },
+        { name: 'Élèves 2A ISUP (I25)', email: 'promo-isupfere25@minesparis.psl.eu' },
+        { name: 'Élèves 3A ISUP (I24)', email: 'promo-isupfere24@minesparis.psl.eu' },
     ];
 
     const contacts = [
@@ -23,11 +23,11 @@ function Home() {
     ];
 
     const vpSoutien = [
-        { name: 'Luigi Romain', email: 'luigi.romain@etu.minesparis.psl.eu', phone: '+33 (0)7 68 30 24 98' },
-        { name: 'Hélory Grenade', email: 'helory.grenade@etu.minesparis.psl.eu', phone: '+33 (0)7 72 33 94 29' },
-        { name: 'Solène Losantos', email: 'solene.losantos@etu.minesparis.psl.eu', phone: '+33 (0)6 25 10 65 26' },
-        { name: 'Baptiste Vial', email: 'baptiste.vial@etu.minesparis.psl.eu', phone: '+33 (0)6 42 67 63 58' },
-        { name: 'Marguerite Tabary', email: 'marguerite.tabary@etu.minesparis.psl.eu', phone: '+33 (0)6 34 42 59 11' },
+        { name: 'Oriane Arnaud', email: 'oriane.arnaud@etu.minesparis.psl.eu', phone: '+33 (0)7 84 26 17 28' },
+        { name: 'Raphaelle Christmann', email: 'raphaelle.christmann@etu.minesparis.psl.eu', phone: '+33 (0)7 67 02 25 59' },
+        { name: 'Pierre Dubrez', email: 'pierre.dubrez@etu.minesparis.psl.eu', phone: '+33 (0)6 58 84 90 95' },
+        { name: 'Inès Gabillé', email: 'ines.gabille@etu.minesparis.psl.eu', phone: '+33 (0)7 81 28 32 54' },
+        { name: 'Christiane Hebey', email: 'christiane.hebey@etu.minesparis.psl.eu', phone: '+33 (0)6 32 34 31 31' },
     ];
 
     const { data, isLoading, isError } = useQuery({
